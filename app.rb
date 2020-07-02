0c32147b77aec4d7c79fe7abe59b3acb2feb54d0
larkro <lars.kronfalt@gmail.com>
2020-07-02 10:25:36 +0200

    WIP

--- app.rb
+++ app.rb
@@ -4,28 +4,32 @@ require "openssl"
 require "securerandom"
 require "digest/sha2"
 require "base64"
+require "singleton"
 
 class PotatoCollection
-  include Enumerable
-
-  attr_accessor :potatoes
+  include Singleton
 
   def initialize
     @@potatoes = {}
   end
 
-  def add potato
+  def add(potato)
     id = generateId
-    # @potatoes.merge!({ generateId => { msg: "msg-1", secret: "secret-1" }})
-    @@potatoes.merge!({id => potato})
+    @@potatoes[id] = potato
     @potato = @@potatoes.to_h[id]
   end
 
-  def self.getPotato id
+  def getPotato(id)
     @potato = @@potatoes.to_h[id]
     @@potatoes.delete(id)
     @potato
   end
+
+  def all
+    @@potatoes
+  end
+
+  private
 
   def generateId
     loop do
@@ -33,14 +37,6 @@ class PotatoCollection
       token = SecureRandom.urlsafe_base64(5)
       break token unless @@potatoes.include?(token)
     end
-  end
-
-  def self.all
-    @@potatoes
-  end
-
-  def each
-    @@potatoes.each { |potato| yield potato }
   end
 end
 
@@ -50,22 +46,23 @@ class HotPotato < Sinatra::Base
   configure do
     set :bind, "0.0.0.0" # Default dev env is localhost only, works bad with containers.
     # set :port, 443 # Uncomment if handling TLS
-    @@potatoes = PotatoCollection.new
+    set :alg, "AES-256-CBC"
   end
 
   def genRandom
     SecureRandom.alphanumeric(10)
   end
 
-  def encryptPotato(secret, potato)
-    alg = "AES-256-CBC"
-    iv = OpenSSL::Cipher.new(alg).random_iv
+  def generateInitializationVector
+    OpenSSL::Cipher.new(settings.alg).random_iv
+  end
 
+  def encryptPotato(secret, potato, iv)
     digest = Digest::SHA256.new
     digest.update(secret)
     key = digest.digest
 
-    aes = OpenSSL::Cipher.new(alg)
+    aes = OpenSSL::Cipher.new(settings.alg)
     aes.encrypt
     aes.key = key
     aes.iv = iv
@@ -76,32 +73,38 @@ class HotPotato < Sinatra::Base
 
   def decryptPotato(secret, potato)
     alg = "AES-256-CBC"
+    key = secret
+    iv = potato[:iv]
+    msg = potato[:msg]
+    p key
+    p iv
+    p msg
     decode_cipher = OpenSSL::Cipher::Cipher.new(alg)
     decode_cipher.decrypt
     decode_cipher.key = key
     decode_cipher.iv = iv
-    plain = decode_cipher.update(cipher64.unpack('m')[0])
+    plain = decode_cipher.update(cipher64.unpack1("m"))
     plain << decode_cipher.final
   end
 
   get "/" do
-    @ttl = { "1 day (24h)" => 86400, "3 days (72h)" => 259200, "7 days" => 604800 }
+    @ttl = {"1 day (24h)" => 86400, "3 days (72h)" => 259200, "7 days" => 604800}
     @default_ttl = "3 days"
     @title = "Add HotPotato"
     @my_secret = genRandom
     erb :index
   end
 
-  get "/test/" do
-    msg = genRandom
-    secret = genRandom
-    @@potatoes.add("{msg: \"msg-#{msg}\", secret: \"secret-#{secret}\"}")
-    erb '<a href="/test/"> do it again!</a>'
-    # redirect to("/")
-  end
+  # get "/test/" do
+  #   msg = genRandom
+  #   secret = genRandom
+  #   @@potatoes.add("{msg: \"msg-#{msg}\", secret: \"secret-#{secret}\"}")
+  #   erb '<a href="/test/"> do it again!</a>'
+  #   # redirect to("/")
+  # end
 
   get "/list/" do
-    PotatoCollection.all.to_s
+    PotatoCollection.instance.all.to_s
   end
 
   post "/addPotato" do
@@ -118,8 +121,9 @@ class HotPotato < Sinatra::Base
       @potato = Base64.encode64(params["potato"])
       @secret = params["secret"]
       @ttl = params["ttl"]
-      @cipher = encryptPotato(@secret, @potato)
-      @my_potato = @@potatoes.add({msg: @potato, secret: @secret})
+      @iv = generateInitializationVector
+      @cipher = encryptPotato(@secret, @potato, @iv)
+      @my_potato = PotatoCollection.instance.add({msg: @cipher, secret: @secret, iv: @iv})
       erb :potato
     end
   end
@@ -128,11 +132,12 @@ class HotPotato < Sinatra::Base
   # @@potatoes.getPotato("{params[:potato]").to_s
   get "/get/:potato" do
     @potato = params["potato"]
-    @p = PotatoCollection.getPotato(@potato).to_h
-    if @p == nil
+    @p = PotatoCollection.instance.getPotato(@potato).to_h
+    if @p.nil?
       redirect to("/")
     else
-      erb '<p>Your potato</p><pre><%= @p[:secret] %></pre></p><pre><%= @p[:msg] %></pre>'
+      # decryptPotato(1,@p)
+      erb "<p>Your potato</p><pre><%= @p[:secret] %></pre></p><pre><%= @p[:msg] %></pre>"
     end
   end
 
