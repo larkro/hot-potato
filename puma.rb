bbeb211a2b8a9c14b17423bd977cd798f583a2e8
larkro <lars.kronfalt@gmail.com>
2020-07-08 14:00:19 +0200

    fix code style

--- puma.rb
+++ puma.rb
@@ -1,8 +1,8 @@
 workers Integer(0)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+threads Integer(ENV["MIN_THREADS"] || 1), Integer(ENV["MAX_THREADS"] || 16)
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 4567
-environment ENV['RACK_ENV'] || 'development'
+rackup DefaultRackup
+port ENV["PORT"] || 4567
+environment ENV["RACK_ENV"] || "development"
